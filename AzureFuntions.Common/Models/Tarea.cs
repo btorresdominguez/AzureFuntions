7cc16a1a47fc69986cc7c0d37b0f1813027d91fd
Beverly Torres Dominguez <beverly.torres@datatools.com.co>
2022-10-19 10:42:15 -0500

    Adicionar entities

---
+++ AzureFuntions.Common/Models/Tarea.cs
@@ -0,0 +1,9 @@
+﻿namespace AzureFuntions.Common.Models
+{
+    public class Tarea
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ AzureFuntions.Functions/Entities/TareaEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFuntions.Functions.Entities
+{
+    public class TareaEntity : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
